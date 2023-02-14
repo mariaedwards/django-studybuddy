b912151fb09c83c14efc39f72ff1b37efdf0e49a
maria.edwards <maria.ana.edwards@gmail.com>
2023-02-13 18:42:10 -0800

    Adds search and filter functionality

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,7 @@ from base import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("rooms/<int:room_id>/", views.room, name="rooms"),
-    path("create-room/", views.create_room, name="create_room")
+    path("create-room/", views.create_room, name="create_room"),
+    path("edit-room/<int:room_id>/", views.edit_room, name="edit_room"),
+    path("delete-room/<int:room_id>/", views.delete_room, name="delete_room")
 ]
--- base/views.py
+++ base/views.py
@@ -1,32 +1,76 @@
 """ Views
 """
 
+from django.http import HttpResponseBadRequest
+from django.db.models import Q
 from django.shortcuts import render, redirect
-from base.models import Room
+from base.models import Room, Topic
 from base.forms import RoomForm
 
 
 def home(request):
     """Home page"""
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html', context)
+    if request.method == 'GET':
+        q = ''
+        if 'q' in request.GET:
+            q = request.GET['q']
+        rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(
+            description__icontains=q) | Q(host__username__icontains=q))
+        room_count = rooms.count()
+        topics = Topic.objects.all()
+        context = {'rooms': rooms, 'topics': topics, "room_count": room_count}
+        return render(request, 'base/home.html', context)
+    else:
+        return HttpResponseBadRequest()
 
 
 def room(request, room_id):
     """Room page"""
-    current_room = Room.objects.get(pk=room_id)
-    context = {"room": current_room}
-    return render(request, 'base/room.html', context)
+    if request.method == 'GET':
+        current_room = Room.objects.get(pk=room_id)
+        context = {"room": current_room}
+        return render(request, 'base/room.html', context)
+    else:
+        return HttpResponseBadRequest()
 
 
 def create_room(request):
     """Create room page"""
-    form = RoomForm()
-    if request.method == 'POST':
+    if request.method == 'GET':
+        form = RoomForm()
+        context = {'form': form}
+        return render(request, 'base/room_form.html', context)
+    elif request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
-    context = {'form': form}
-    return render(request, 'base/room_form.html', context)
+    else:
+        return HttpResponseBadRequest()
+
+
+def edit_room(request, room_id):
+    """Edit room page"""
+    current_room = Room.objects.get(pk=room_id)
+    if request.method == 'GET':
+        form = RoomForm(instance=current_room)
+        context = {'form': form}
+        return render(request, 'base/room_form.html', context)
+    elif request.method == 'POST':
+        form = RoomForm(request.POST, instance=current_room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        return HttpResponseBadRequest()
+
+
+def delete_room(request, room_id):
+    """Delete room page"""
+    current_room = Room.objects.get(pk=room_id)
+    if request.method == 'GET':
+        return render(request, 'base/delete.html', {"obj": current_room})
+
+    if request.method == 'POST':
+        current_room.delete()
+        return redirect('home')
