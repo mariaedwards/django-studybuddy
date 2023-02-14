565833614b099c1b82e07d3803ad281542263ebf
maria.edwards <maria.ana.edwards@gmail.com>
2023-02-13 17:42:35 -0800

    Adds a form and default ordering to Rooms

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ from base import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("rooms/<int:room_id>/", views.room, name="rooms")
+    path("rooms/<int:room_id>/", views.room, name="rooms"),
+    path("create-room/", views.create_room, name="create_room")
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 """ Views
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from base.models import Room
+from base.forms import RoomForm
 
 
 def home(request):
@@ -17,3 +18,15 @@ def room(request, room_id):
     current_room = Room.objects.get(pk=room_id)
     context = {"room": current_room}
     return render(request, 'base/room.html', context)
+
+
+def create_room(request):
+    """Create room page"""
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
